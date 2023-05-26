d1f2891de267da1f7a51e0cc8126f107638d408b
RamiroRojasBociga <ramiro.rojasb@uqvirtual.edu.co>
2023-05-26 18:49:29 -0500

    se crea la clase camion

---
+++ Proyecto_Tu_Carro_Final/src/trabajo_Final/Camion.java
@@ -0,0 +1,22 @@
+package trabajo_Final;
+
+public class Camion extends Vehiculo {
+
+	
+	
+
+	public Camion(String marca, String modelo, int cambios, int velocidadMaxima, int cilindraje, String placa,
+			String fotografias, estado tipoEstado, abs tipoAbs, combustible tipoCombustible,
+			transmision tipoTransmision) {
+		super(marca, modelo, cambios, velocidadMaxima, cilindraje, placa, fotografias,
+				tipoEstado, tipoAbs, tipoCombustible,tipoTransmision);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
