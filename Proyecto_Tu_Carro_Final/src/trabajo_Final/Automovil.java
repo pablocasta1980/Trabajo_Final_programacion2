6f632f7eae0dfbf1a321893c3d0eb0623b97230d
RamiroRojasBociga <ramiro.rojasb@uqvirtual.edu.co>
2023-05-26 12:21:44 -0500

    Se crea la clase Moto

--- Proyecto_Tu_Carro_Final/src/trabajo_Final/Automovil.java
+++ Proyecto_Tu_Carro_Final/src/trabajo_Final/Automovil.java
@@ -7,18 +7,21 @@ public abstract class Automovil extends Vehiculo {
 	private int numeroPuertas;
 	private int numeroBolsas;
 	public enum  TieneCamara {SI, NO};
-	public enum  TieneSensor {SI, NO};
+	public enum  TieneSensorColision {SI, NO};
 	public enum  TieneSensorTrafico {SI, NO};
-	public enum  AsistentePersonal {SI, NO};
+	public enum  AsistentePermanencia {SI, NO};
 	public enum  AireAcondicionado {SI, NO};
 	
 	public Automovil(String marca, String modelo, int cambios, int velocidadMaxima, int cilindraje,
 			String fotografias, int numeroPasajeros, int numeroPuertas, int numeroBolsas, 
-			TieneCamara tieneCam,TieneSensor tieneSen,TieneSensorTrafico tieneSenTraf, AsistentePersonal asistentePer,
+			TieneCamara tieneCam,TieneSensorColision tieneSenCol,TieneSensorTrafico tieneSenTraf, AsistentePermanencia asistentePer,
 			AireAcondicionado aireAcond) {
 		super(marca, modelo, cambios, velocidadMaxima, cilindraje, fotografias);
 		// TODO Auto-generated constructor stub
+		
 	}
+	@Override
+	public abstract String toString ();
 	
 
 }
---
+++ Proyecto_Tu_Carro_Final/src/trabajo_Final/Moto.java
@@ -0,0 +1,20 @@
+package trabajo_Final;
+
+public class Moto extends Vehiculo {
+
+	public Moto(String marca, String modelo, int cambios, int velocidadMaxima, int cilindraje, String fotografias) {
+		super(marca, modelo, cambios, velocidadMaxima, cilindraje, fotografias);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+	
+
+}
