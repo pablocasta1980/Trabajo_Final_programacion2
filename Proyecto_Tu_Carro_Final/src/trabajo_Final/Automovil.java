b52e270f080005a293ec997718647fd32b9280d7
RamiroRojasBociga <ramiro.rojasb@uqvirtual.edu.co>
2023-05-26 12:05:08 -0500

    Se crea la clase automovil

--- Proyecto_Tu_Carro_Final/src/trabajo_Final/Automovil.java
+++ Proyecto_Tu_Carro_Final/src/trabajo_Final/Automovil.java
@@ -1,5 +1,24 @@
 package trabajo_Final;
 
 public abstract class Automovil extends Vehiculo {
+	
+	
+	private int numeroPasajeros;
+	private int numeroPuertas;
+	private int numeroBolsas;
+	public enum  TieneCamara {SI, NO};
+	public enum  TieneSensor {SI, NO};
+	public enum  TieneSensorTrafico {SI, NO};
+	public enum  AsistentePersonal {SI, NO};
+	public enum  AireAcondicionado {SI, NO};
+	
+	public Automovil(String marca, String modelo, int cambios, int velocidadMaxima, int cilindraje,
+			String fotografias, int numeroPasajeros, int numeroPuertas, int numeroBolsas, 
+			TieneCamara tieneCam,TieneSensor tieneSen,TieneSensorTrafico tieneSenTraf, AsistentePersonal asistentePer,
+			AireAcondicionado aireAcond) {
+		super(marca, modelo, cambios, velocidadMaxima, cilindraje, fotografias);
+		// TODO Auto-generated constructor stub
+	}
+	
 
 }
