9b28b44121485408628dcee127d793a747984e09
RamiroRojasBociga <ramiro.rojasb@uqvirtual.edu.co>
2023-05-26 17:49:39 -0500

    Se modifica vehiculo, se agregan enum

--- Proyecto_Tu_Carro_Final/src/trabajo_Final/Vehiculo.java
+++ Proyecto_Tu_Carro_Final/src/trabajo_Final/Vehiculo.java
@@ -8,19 +8,24 @@ public abstract class Vehiculo {
 	private int cambios;
 	private int velocidadMaxima;
 	private int cilindraje;
-	public enum Estado {NUEVO, USADO};
-	public enum Abs {SI, NO};
-	public enum Combustible {GASOLINA, DIESEL, ELECTRICO, HIBRIDO};
-	public enum Transmision {AUTOMATICA, MANUAL};
+	private String placas;
 	private String fotografias;
+	public enum estado {NUEVO, USADO};
+	public enum abs {SI, NO};
+	public enum combustible {GASOLINA, DIESEL, ELECTRICO, HIBRIDO};
+	public enum transmision {AUTOMATICA, MANUAL};
 	
-	public Vehiculo(String marca, String modelo, int cambios, int velocidadMaxima, int cilindraje, String fotografias) {
+	
+	public Vehiculo(String marca, String modelo, int cambios, int velocidadMaxima, int cilindraje,
+			String placa, String fotografias,estado tipoEstado, abs tipoAbs, combustible tipoCombustible,
+			transmision tipoTransmision) {
 		super();
 		this.marca = marca;
 		this.modelo = modelo;
 		this.cambios = cambios;
 		this.velocidadMaxima = velocidadMaxima;
 		this.cilindraje = cilindraje;
+		this.placa = placa;
 		this.fotografias = fotografias;
 	}
 	
