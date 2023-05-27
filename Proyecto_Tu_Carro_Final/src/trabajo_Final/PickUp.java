b185925b7b910d101e62aca575ad4d7f1b7b97ae
RamiroRojasBociga <ramiro.rojasb@uqvirtual.edu.co>
2023-05-26 20:54:23 -0500

    se crea la clase Pickup

---
+++ Proyecto_Tu_Carro_Final/src/trabajo_Final/PickUp.java
@@ -0,0 +1,66 @@
+package trabajo_Final;
+
+
+public class PickUp extends Automovil{
+	
+private int capacidadCarga;
+	
+	public enum cuatroPorCuatro {SI, NO};
+	private cuatroPorCuatro esCuatroPorCuatro;
+	
+	
+	public PickUp(String marca, String modelo, int cambios, int velocidadMaxima,
+			int cilindraje, String placa,String fotografias, estado tipoEstado, abs tipoAbs, 
+			combustible tipoCombustible,transmision tipoTransmision, int numeroPasajeros, 
+			int numeroPuertas, int numeroBolsas, camara tieneCamara,
+			sensorColision tieneSensorCol, sensorTrafico tieneSensorTraf, 
+			asistentePermanencia tieneAsistentePerma,aireAcondicionado tieneAireAcond,
+			int capacidadCarga, cuatroPorCuatro esCuatroPorCuatro) {
+		
+		
+		super(marca, modelo, cambios, velocidadMaxima, cilindraje, placa, fotografias, tipoEstado, 
+				tipoAbs, tipoCombustible,tipoTransmision, numeroPasajeros, numeroPuertas, 
+				numeroBolsas, tieneCamara, tieneSensorCol, tieneSensorTraf,
+				tieneAsistentePerma, tieneAireAcond);
+		
+		this.capacidadCarga = capacidadCarga;
+		this.esCuatroPorCuatro = esCuatroPorCuatro;
+		
+	}
+
+
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+		
+		
+	}
+
+
+
+	public int getCapacidadCarga() {
+		return capacidadCarga;
+	}
+
+
+
+	public void setCapacidadCarga(int capacidadCarga) {
+		this.capacidadCarga = capacidadCarga;
+	}
+
+
+
+	public cuatroPorCuatro getEsCuatroPorCuatro() {
+		return esCuatroPorCuatro;
+	}
+
+
+
+	public void setEsCuatroPorCuatro(cuatroPorCuatro esCuatroPorCuatro) {
+		this.esCuatroPorCuatro = esCuatroPorCuatro;
+	}
+	
+
+}
