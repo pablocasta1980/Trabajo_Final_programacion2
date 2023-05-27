ea7ff53bf99f45d28d19516e9676948bdacc300c
RamiroRojasBociga <ramiro.rojasb@uqvirtual.edu.co>
2023-05-26 20:15:21 -0500

    se crea la clase sedan

--- Proyecto_Tu_Carro_Final/src/trabajo_Final/Camion.java
+++ Proyecto_Tu_Carro_Final/src/trabajo_Final/Camion.java
@@ -3,6 +3,8 @@ package trabajo_Final;
 public class Camion extends Vehiculo {
 	
 	
+	private int capacidadCarga;
+	private int numeroEjes;
 	
 	public enum tipoCamion {VOLQUETA, REMOLQUE, TANQUE};
 	private tipoCamion tipoCam;
@@ -13,11 +15,14 @@ public class Camion extends Vehiculo {
 	
 	public Camion(String marca, String modelo, int cambios, int velocidadMaxima, int cilindraje, String placa,
 			String fotografias, estado tipoEstado, abs tipoAbs, combustible tipoCombustible,
-			transmision tipoTransmision, tipoCamion tipoCam, frenoAire tieneFrenoAire) {
+			transmision tipoTransmision,int capacidadCarga, int numeroEjes,
+			tipoCamion tipoCam, frenoAire tieneFrenoAire) {
 		
 		super(marca, modelo, cambios, velocidadMaxima, cilindraje, placa, fotografias,
 				tipoEstado, tipoAbs, tipoCombustible,tipoTransmision);
 		
+		this.capacidadCarga = capacidadCarga;
+		this.numeroEjes = numeroEjes;
 		this.tipoCam = tipoCam;
 		this.tieneFrenoAire = tieneFrenoAire;
 		
@@ -27,6 +32,38 @@ public class Camion extends Vehiculo {
 	public String toString() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public int getCapacidadCarga() {
+		return capacidadCarga;
+	}
+
+	public void setCapacidadCarga(int capacidadCarga) {
+		this.capacidadCarga = capacidadCarga;
+	}
+
+	public int getNumeroEjes() {
+		return numeroEjes;
+	}
+
+	public void setNumeroEjes(int numeroEjes) {
+		this.numeroEjes = numeroEjes;
+	}
+
+	public tipoCamion getTipoCam() {
+		return tipoCam;
+	}
+
+	public void setTipoCam(tipoCamion tipoCam) {
+		this.tipoCam = tipoCam;
+	}
+
+	public frenoAire getTieneFrenoAire() {
+		return tieneFrenoAire;
+	}
+
+	public void setTieneFrenoAire(frenoAire tieneFrenoAire) {
+		this.tieneFrenoAire = tieneFrenoAire;
 	}
 
 }
